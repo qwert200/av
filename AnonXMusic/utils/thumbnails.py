392ed0711645c10f09ad48b00f00c58733b64800
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-08-23 11:33:02 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -75,7 +75,7 @@ async def get_thumb(videoid):
         draw = ImageDraw.Draw(background)
         arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 30)
         font = ImageFont.truetype("AnonXMusic/assets/font.ttf", 30)
-        draw.text((1110, 8), unidecode(app.name), fill="red", font=arial)
+        draw.text((1100, 8), unidecode(app.name), fill="white", font=font)
         draw.text(
             (55, 560),
             f"{channel} | {views[:23]}",
