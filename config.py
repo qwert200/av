3707d24789ed9f03ed26f05c939e1c95a76170f7
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-08-23 08:12:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,18 +84,18 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://telegra.ph/file/84f616710b53e4cac208d.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/84f616710b53e4cac208d.jpg"
+    "PING_IMG_URL", "https://telegra.ph/file/7fd3c67bca3f72ab70edd.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/748b10cdb7e21b5dae44b.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/cb512c35937ab7c5aedc5.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/134923b0408d4a08f35b8.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/134923b0408d4a08f35b8.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/24d268b14b19f9832e70a.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/581bd40bf7d495d2353f4.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/62b72a1bea8deae9a59fb.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/2de154a22f5b66e594a77.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/008030ab5784f6ce8ae43.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/008030ab5784f6ce8ae43.jpg"
 
 
 def time_to_seconds(time):
