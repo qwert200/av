96f2eea8c70405756a3912b0791bc4abf6462b44
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-08-27 15:15:09 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -72,11 +72,10 @@ async def get_thumb(videoid):
         background = enhancer.enhance(0.5)
         draw = ImageDraw.Draw(background)
         arial = ImageFont.truetype("AnonXMusic/assets/font2.ttf", 30)
-        font = ImageFont.truetype("AnonXMusic/assets/font.ttf", 40)
-        font2 = ImageFont.truetype("AnonXMusic/assets/font.ttf", 30)
+        font = ImageFont.truetype("AnonXMusic/assets/font.ttf", 30)
         draw.text((1100, 8), unidecode(app.name), fill="white", font=font)
         draw.text(
-            (70, 560),
+            (55, 560),
             f"{channel} | {views[:23]}",
             (255, 255, 255),
             font=font,
@@ -103,13 +102,13 @@ async def get_thumb(videoid):
             (36, 670),
             "00:00",
             (255, 255, 255),
-            font=font2,
+            font=font,
         )
         draw.text(
             (1185, 670),
             f"{duration[:23]}",
             (255, 255, 255),
-            font=font2,
+            font=font,
         )
         try:
             os.remove(f"cache/thumb{videoid}.png")
