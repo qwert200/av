89cf446a0712a7a58f324a82fb87d3238b5ad108
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2023-08-29 14:38:00 +0530

    Update thumbnails.py

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -111,7 +111,7 @@ async def get_thumb(videoid):
             font=arial,
         )
         draw.text(
-            (1165, 670),
+            (1150, 670),
             f"{duration[:23]}",
             (255, 255, 255),
             stroke_width=4,
