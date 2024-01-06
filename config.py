86f80c02245f00c1e29f9b7e3bd19da38cf101be
Sangram <136038979+OfficialSangram@users.noreply.github.com>
2024-01-06 08:24:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org//file/81a11077138f4eb9b51db.jpg"
+    "START_IMG_URL", "https://telegra.ph/file/c281d14d16bca21077e8b.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/7fd3c67bca3f72ab70edd.jpg"
